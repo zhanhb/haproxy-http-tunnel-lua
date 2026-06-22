5103057509b44c2330dcb9516482d254b6a8abda
zhanhb <6323014+zhanhb@users.noreply.github.com>
2026-06-23 02:14:43 +0800

    remove reason phrase
    The status reason is not needed at all. For haproxy will generate these for us.

--- http-connect.lua
+++ http-connect.lua
@@ -9,62 +9,17 @@ local function must_regex(regex, case_sensitive)
     error(format("regex='%s', reason='%s'", regex, res), 2)
 end
 
-local reasons = {
-    [400] = 'Bad Request',
-    [401] = 'Unauthorized',
-    [402] = 'Payment Required',
-    [403] = 'Forbidden',
-    [404] = 'Not Found',
-    [405] = 'Method Not Allowed',
-    [406] = 'Not Acceptable',
-    [407] = 'Proxy Authentication Required',
-    [408] = 'Request Timeout',
-    [409] = 'Conflict',
-    [410] = 'Gone',
-    [411] = 'Length Required',
-    [412] = 'Precondition Failed',
-    [413] = 'Content Too Large',
-    [414] = 'URI Too Long',
-    [415] = 'Unsupported Media Type',
-    [416] = 'Range Not Satisfiable',
-    [417] = 'Expectation Failed',
-    [418] = "I'm a teapot",
-    [421] = 'Misdirected Request',
-    [422] = 'Unprocessable Content',
-    [423] = 'Locked',
-    [424] = 'Failed Dependency',
-    [425] = 'Too Early',
-    [426] = 'Upgrade Required',
-    [428] = 'Precondition Required',
-    [429] = 'Too Many Requests',
-    [431] = 'Request Header Fields Too Large',
-    [451] = 'Unavailable For Legal Reasons',
-    [500] = 'Internal Server Error',
-    [501] = 'Not Implemented',
-    [502] = 'Bad Gateway',
-    [503] = 'Service Unavailable',
-    [504] = 'Gateway Timeout',
-    [505] = 'HTTP Version Not Supported',
-    [506] = 'Variant Also Negotiates',
-    [507] = 'Insufficient Storage',
-    [508] = 'Loop Detected',
-    [510] = 'Not Extended',
-    [511] = 'Network Authentication Required',
-}
-
 local function common_res(txn, ret, status, ...)
     local cat = core.concat()
     cat:add('HTTP/1.1 ')
     if type(status) ~= 'string' then
         cat:add(tostring(status))
         cat:add(' ')
-        cat:add(reasons[status] or '')
     elseif #status > 3 then
         cat:add(status)
     else
         cat:add(status)
         cat:add(' ')
-        cat:add(reasons[tonumber(status)] or '')
     end
     cat:add('\r\nDate: ')
     cat:add(txn.sc:http_date(txn.f:date()))
